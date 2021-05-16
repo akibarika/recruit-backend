6366f46d911aef63d6f7045635737144a1a6f660
Akibarika <lxclxc89816@gmail.com>
2021-05-17 03:09:52 +1200

    Init for MongoDB connect

---
+++ CreditCardForm/CreditCardForm.Model/CreditCardFormDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace CreditCardForm.Model
+{
+    public class CreditCardFormDatabaseSettings : ICreditCardFormDatabaseSettings
+    {
+        public string CollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface ICreditCardFormDatabaseSettings
+    {
+        string CollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
