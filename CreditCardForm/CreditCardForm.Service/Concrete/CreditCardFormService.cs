463af53d0f2f3ab2edae7a17b33b23f9904b2bd7
Akibarika <lxclxc89816@gmail.com>
2021-05-17 03:11:37 +1200

    Create Service project

---
+++ CreditCardForm/CreditCardForm.Service/Concrete/CreditCardFormService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CreditCardForm.DataAccess.Interface;
+using CreditCardForm.Model;
+using CreditCardForm.Service.Interface;
+
+namespace CreditCardForm.Service.Concrete
+{
+    public class CreditCardFormService : ICreditCardFormService
+    {
+        private ICreditCardFormRepository _repo;
+
+        public CreditCardFormService(ICreditCardFormRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<CreditCard> AddNewCreditCard(CreditCard card)
+        {
+            var creditCard = await _repo.Insert(card);
+            return creditCard;
+        }
+
+        public async Task<CreditCard> Get(Guid id)
+        {
+            var creditCard = await _repo.Get(id);
+            return creditCard;
+        }
+
+        public async Task<IList<CreditCard>> GetAll()
+        {
+            var creditCards = await _repo.GetAll();
+            return creditCards;
+        }
+    }
+}
---
+++ CreditCardForm/CreditCardForm.Service/Interface/ICreditCardFormService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CreditCardForm.Model;
+
+
+namespace CreditCardForm.Service.Interface
+{
+    public interface ICreditCardFormService
+    {
+        Task<IList<CreditCard>> GetAll();
+        Task<CreditCard> Get(Guid id);
+        Task<CreditCard> AddNewCreditCard(CreditCard card);
+    }
+}
